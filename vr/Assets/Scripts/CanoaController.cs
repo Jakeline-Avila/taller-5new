64bc3bd92d5e5b6923002063c650f68284c1e6d9
Villegazs <133931476+Villegazs@users.noreply.github.com>
2025-11-06 15:29:02 -0500

    Gira
    toca arreglar un error

---
+++ vr/Assets/Scripts/CanoaController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CanoaController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Este método se llama cuando entra a un collider marcado como trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Volteo"))
+        {
+            VoltearCanoa();
+        }
+    }
+
+    private void VoltearCanoa()
+    {
+        // Rotación instantánea
+        transform.Rotate(0, 80, 180f);
+
+        // O puedes usar física:
+        // rb.AddTorque(transform.right * 500f, ForceMode.Impulse);
+    }
+}
